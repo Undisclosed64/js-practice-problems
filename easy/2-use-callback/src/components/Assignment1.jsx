f34941d983835dea42286a484847bbec780c44db
undisclosed64 <undisclosed64@gmail.com>
2024-01-31 17:21:50 +0600

    add usecallback tasks

---
+++ easy/2-use-callback/src/components/Assignment1.jsx
@@ -0,0 +1,30 @@
+import { useCallback, useState } from "react";
+
+// Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
+
+export function Assignment1() {
+  const [count, setCount] = useState(0);
+
+  // Your code starts here
+  const increment = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
+
+  const decrement = useCallback(() => {
+    setCount(count - 1);
+  }, [count]);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <CounterButtons onIncrement={increment} onDecrement={decrement} />
+    </div>
+  );
+}
+
+const CounterButtons = ({ onIncrement, onDecrement }) => (
+  <div>
+    <button onClick={onIncrement}>Increment</button>
+    <button onClick={onDecrement}>Decrement</button>
+  </div>
+);
